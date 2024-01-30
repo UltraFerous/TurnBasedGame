b105421e254e3067144018297c760e29fa4c49ef
UltraFerous <ultraferous@gmail.com>
2024-01-29 20:00:52 -0500

    Added winner check condition

--- src/components/Combat.jsx
+++ src/components/Combat.jsx
@@ -1,48 +1,72 @@
-import { useContext, useState } from "react";
+import { useContext, useState, useEffect } from "react";
 import PlayerContext from "../context/playerContext";
 import EnemyContext from "../context/enemyContext";
 import { attackRoll } from "../helpers/attack";
 
 function Combat() {
   const [turn, setTurn] = useState(0);
+  const [battleOver, setbattleOver] = useState(false);
   const { player, setPlayer } = useContext(PlayerContext);
   const { enemy, setEnemy } = useContext(EnemyContext);
 
-  const playerTurnManager = function () {
-    const updatedEnemy = {
-      ...enemy,
-      stats: {
-        ...enemy.stats,
-        currentWounds: attackRoll(player, enemy),
-      },
-    };
-    setEnemy(updatedEnemy)
-    setTurn(1);
+  const checkIfCombatIsOver = async function (attacker, defender, setbattleOver) {
+    if (attacker.stats.currentWounds < 1 || defender.stats.currentWounds < 1) {
+      return setbattleOver(true);
+    }
   };
 
-  const enemyTurnManager = function () {
-    const updatedPlayer = {
-      ...player,
+  const calculateDamage = function (attacker, defender) {
+    const updatedStats = {
+      ...defender,
       stats: {
-        ...player.stats,
-        currentWounds: attackRoll(enemy, player),
+        ...defender.stats,
+        currentWounds: attackRoll(attacker, defender),
       },
     };
-    setPlayer(updatedPlayer);
-    setTurn(0);
+    return updatedStats;
   };
+
+  const turnManager = async function (
+    attacker,
+    defender,
+    setAttacker,
+    setDefender
+  ) {
+    try {
+      const damageDone = calculateDamage(attacker, defender);
+      await setDefender(damageDone);
+      if (!battleOver) {
+        turn === 0 ? setTurn(1) : setTurn(0);
+      }
+    } catch {
+      console.error("Error in turnManager:", error);
+    }
+  };
+
+  useEffect(() => {
+    checkIfCombatIsOver(player, enemy, setbattleOver);
+  }, [player, enemy]);
 
   return (
     <div>
       <div>It is turn: {turn === 0 ? "player" : "enemy"}.</div>
       <div>Player Health: {player.stats.currentWounds}</div>
       <div>Enemy Health: {enemy.stats.currentWounds}</div>
-      <button
-        type="submit"
-        onClick={turn === 0 ? playerTurnManager : enemyTurnManager}
-      >
-        Change Turn
-      </button>
+      {battleOver === false ? (
+        <button
+          type="submit"
+          onClick={() => {
+            turn === 0
+              ? turnManager(player, enemy, setPlayer, setEnemy)
+              : turnManager(enemy, player, setEnemy, setPlayer);
+          }}
+        >
+          Change Turn
+        </button>
+      ) : (
+        // Because the turn changes before the combat ends we check if turn is equal to 1
+        <div>Battle Over {turn === 1 ? "Player Wins!" : "Enemy Wins!"}</div>
+      )}
     </div>
   );
 }
--- src/helpers/attack.js
+++ src/helpers/attack.js
@@ -5,6 +5,7 @@ import {
   woundComparison,
 } from "../helpers/diceRolls";
 
+// Rolls the dice to hit, looking for results above or equal to the weapon skill
 const hitRoll = function (user) {
   return filterDicePoolAbove(
     rollXDice(user.weapons[0].attacks),
@@ -12,6 +13,7 @@ const hitRoll = function (user) {
   );
 };
 
+// Rolls the dice to wound, looking for results above or equal to the comparison result
 const woundRoll = function (user, target, rolls) {
   const woundTargetNumber = woundComparison(
     user.stats.strength + user.weapons[0].strengthBonus,
@@ -20,6 +22,7 @@ const woundRoll = function (user, target, rolls) {
   return filterDicePoolAbove(rollXDice(rolls), woundTargetNumber);
 };
 
+// Rolls the dice to save, looking for failed saves which are results less than the modified armour
 const saveRoll = function (user, target, rolls) {
   return filterDicePoolBelow(
     rollXDice(rolls),
@@ -35,25 +38,27 @@ const attackRoll = function (user, target) {
   let successfulRolls = 0;
 
   // Roll to hit, roll amount of attacks above or equal to skill
-  const hitRollResults = hitRoll(user)
+  const hitRollResults = hitRoll(user);
   successfulRolls = hitRollResults.length;
   console.log("Hit roll", successfulRolls);
 
   // Roll to wound, roll above comparison, unit strength + weapon vs target toughness
-  const woundRollResults = woundRoll(user, target, successfulRolls)
+  const woundRollResults = woundRoll(user, target, successfulRolls);
   successfulRolls = woundRollResults.length;
   console.log("Wound roll", successfulRolls);
 
   //Target rolls to save, armour + rend, roll above or equal to target
-  const saveRollResults = saveRoll(user, target, successfulRolls)
+  const saveRollResults = saveRoll(user, target, successfulRolls);
   successfulRolls = saveRollResults.length;
   console.log("Save roll", successfulRolls);
 
   //Reduce target wounds equal to the weapons damage
-  const targetDamageResults = damageRoll(successfulRolls, user)
+  const targetDamageResults = damageRoll(successfulRolls, user);
   console.log("Damage roll", targetDamageResults);
 
-  return target.stats.currentWounds - targetDamageResults;
+  //Returns the new health of the target
+  const newCurrentWounds = target.stats.currentWounds - targetDamageResults;
+  return newCurrentWounds;
 };
 
 export { attackRoll };
