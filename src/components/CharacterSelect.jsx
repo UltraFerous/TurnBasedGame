cbaf0d0e4a1370cf371bc607dfe8dca3733b87bb
UltraFerous <ultraferous@gmail.com>
2024-03-18 19:03:12 -0400

    Added another endgame screen and updated character select funtionality

--- src/components/CharacterSelect.jsx
+++ src/components/CharacterSelect.jsx
@@ -1,14 +1,20 @@
-import { React, useContext } from "react";
+import { React, useContext, useState } from "react";
 import PlayerContext from "../context/playerContext";
 import heroDatabase from "../db/heroDatabase";
 import "../styles/CharacterSelect.scss";
 
 function CharcterSelect() {
   const { player, setPlayer } = useContext(PlayerContext);
+  const [tempPlayer, setTempPlayer] = useState();
 
-  const handleSelectChange = (event) => {
+  const handleCharacterSelect = (event) => {
     console.log("Changed character to:", event);
-    setPlayer(heroDatabase[event]);
+    setTempPlayer(heroDatabase[event]);
+  };
+
+  const handleReadyCharacter = (event) => {
+    console.log("Locked character to:", tempPlayer);
+    setPlayer(tempPlayer);
   };
 
   return (
@@ -16,14 +22,19 @@ function CharcterSelect() {
       <div className="CharacterInformation">
         <div className="CharacterDescription"></div>
         <div className="CharacterImage">
-          <button>READY</button>
+          <button
+            className="ReadyCharacterButton"
+            onClick={() => handleReadyCharacter()}
+          >
+            READY
+          </button>
         </div>
       </div>
       <div className="CharacterCards">
         {heroDatabase.map((hero, index) => (
           <button
             onClick={() => {
-              handleSelectChange(index);
+              handleCharacterSelect(index);
             }}
             value={index}
             key={index}
--- src/components/EndGame.jsx
+++ src/components/EndGame.jsx
@@ -1,18 +1,41 @@
-import { React, useContext } from "react";
+import { React, useContext, useState } from "react";
 import PlayerContext from "../context/playerContext";
 import "../styles/EndGame.scss";
 
 function EndGame() {
   const { player, setPlayer } = useContext(PlayerContext);
+  const [screen, setScreen] = useState(0);
 
-  return (
-    <div className="EndGameScreen">
-      <h1> Game Over! </h1>
-      <div> Your final score was {player.scores.points}. </div>
-      <div> You made it to stage {player.scores.stage}! </div>
-      <div> You had {player.scores.money} dollars! </div>
-    </div>
-  );
+  const changeScreen = function () {
+    screen === 0 ? setScreen(1) : setScreen(0);
+  };
+
+  if (screen === 0) {
+    return (
+      <div className="EndGameScreen">
+        <button onClick={() => changeScreen()}> Change </button>
+        <h1> Game Over! </h1>
+        <div> Your final score was {player.scores.points}. </div>
+        <div> You made it to stage {player.scores.stage}! </div>
+        <div> You had {player.scores.money} dollars! </div>
+      </div>
+    );
+  }
+
+  if (screen === 1) {
+    return (
+      <div className="EndGameScreen">
+        <button onClick={() => changeScreen()}> Change </button>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+        mollit anim id est laborum.
+      </div>
+    );
+  }
 }
 
 export default EndGame;
